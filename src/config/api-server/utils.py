d2a46efb1e11b2526c9313eb56c1b216f03dcdce
Jalandip <jalandip@juniper.net>
2016-03-09 19:05:20 -0800

    sem-ver: bugfix add default lookup location

--- src/config/api-server/utils.py
+++ src/config/api-server/utils.py
@@ -299,7 +299,7 @@ def parse_args(args_str):
         help="Disable if map")
     parser.add_argument(
         "--service_lookup_file",
-        help="Service lookup yaml file")
+        help="Service lookup yaml file", default='/opt/csp-service/lookup.yaml')
     args_obj, remaining_argv = parser.parse_known_args(remaining_argv)
     args_obj.config_sections = config
     if type(args_obj.cassandra_server_list) is str:
